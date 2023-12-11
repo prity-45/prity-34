feb9150b29f4e872d4d3ca45d03b4346d7d8868c
PRITY KUMARI <152247293+prity-45@users.noreply.github.com>
2023-12-11 05:06:04 +0000

    file added

---
+++ 02-intermediate/arrowFunction.js
@@ -0,0 +1,22 @@
+/* arrow function->
+arrow function also called fat function is a feature in ES6 in js to write consizee function n js*/
+
+// function timeTwo(params){return params*2}
+// function timeTwo(params){
+
+// }
+// console.log(timeTwo(4));
+
+
+// var timeTwo = params => params*2
+//  console.log(timeTwo(4));
+
+ /* 
+
+ dom
+ event
+ async
+ api request
+ promise
+ call back
+ closer*/
