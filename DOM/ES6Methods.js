11cb64a7827a3788ea143bb98b6a5a9f6fed1d6e
PRITY KUMARI <152247293+prity-45@users.noreply.github.com>
2023-12-12 04:48:42 +0000

    file added

---
+++ DOM/ES6Methods.js
@@ -0,0 +1,25 @@
+//filter method-
+
+
+const myArray = [1,2,3,4,5,6,7,8]
+/*const myFilterArray = myArray.filter((item) => {
+      return item > 4
+})
+console.log(myFilterArray);*/
+
+/*const filterArray = myArray.forEach((data)=> {
+    if(data>5){
+        return data
+    }
+})
+console.log(filterArray);
+*/
+
+/*const filterArray = myArray.forEach((data)=> {
+    if(data>5){
+        console.log(data)
+    }
+})*/
+
+
+
