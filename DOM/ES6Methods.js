fa23f2e7609869b7b6fa637f53592eb6ab59787b
PRITY KUMARI <152247293+prity-45@users.noreply.github.com>
2023-12-12 05:31:26 +0000

    file added

--- DOM/ES6Methods.js
+++ DOM/ES6Methods.js
@@ -1,8 +1,8 @@
 //filter method-
 
 
-const myArray = [1,2,3,4,5,6,7,8]
-/*const myFilterArray = myArray.filter((item) => {
+/*const myArray = [1,2,3,4,5,6,7,8]
+const myFilterArray = myArray.filter((item) => {
       return item > 4
 })
 console.log(myFilterArray);*/
@@ -23,3 +23,41 @@ console.log(filterArray);
 
 
 
+//MAP MATHOD 
+
+/*const arrayNum =[2,4,6,8,10]
+const changeArray = arrayNum.map((value) => {
+    return value + 2;
+})
+console.log(changeArray); */
+
+
+//chaining
+
+/*const chainingArray = [3,4,6,7,8,9,10]
+const updateArray = chainingArray
+                             .map((data) => data + 10)
+                             .map((data) => data *10)
+                             .filter((data) => data>150)
+ console.log(updateArray);    */
+ 
+ 
+ const arrofNum = [1,2,3,4,5]
+//  const reduceArray = arrofNum.reduce(function(accumulator , currentValue){
+//     console.log(`accumulator ${accumulator} and ${currentValue}`)
+//     return accumulator+currentValue
+//  },5)
+//  console.log(`our final valie = ${reduceArray}`);
+
+
+
+
+ //reduce method using arrow funtion
+
+ const modifiedArray = arrofNum.reduce((acc , currval) => acc + currval , 0)
+ console.log(`our reduce array = ${modifiedArray}`);
+
+
+ const books ={
+    { title : 'Book one',genre:'fiction',pub}
+ }
