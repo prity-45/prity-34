986bead57bc9162058bca5aeb66da85c112c656c
PRITY KUMARI <152247293+prity-45@users.noreply.github.com>
2023-12-12 05:42:45 +0000

    file added

--- DOM/ES6Methods.js
+++ DOM/ES6Methods.js
@@ -42,7 +42,7 @@ const updateArray = chainingArray
  console.log(updateArray);    */
  
  
- const arrofNum = [1,2,3,4,5]
+//  const arrofNum = [1,2,3,4,5]
 //  const reduceArray = arrofNum.reduce(function(accumulator , currentValue){
 //     console.log(`accumulator ${accumulator} and ${currentValue}`)
 //     return accumulator+currentValue
@@ -54,10 +54,53 @@ const updateArray = chainingArray
 
  //reduce method using arrow funtion
 
- const modifiedArray = arrofNum.reduce((acc , currval) => acc + currval , 0)
- console.log(`our reduce array = ${modifiedArray}`);
+//  const modifiedArray = arrofNum.reduce((acc , currval) => acc + currval , 0)
+//  console.log(`our reduce array = ${modifiedArray}`);
 
 
- const books ={
-    { title : 'Book one',genre:'fiction',pub}
- }
+ const books =[
+    { title : 'Book one',genre:'fiction',publish:1981,edition:2004},
+    { title : 'Book two',genre:'fiction',publish:1981,edition:2004},
+    { title : 'Book three',genre:'fiction',publish:1981,edition:2004},
+    { title : 'Book four',genre:'fiction',publish:1981,edition:2004},
+    { title : 'Book five',genre:'fiction',publish:1981,edition:2004},
+    { title : 'Book six',genre:'fiction',publish:1981,edition:2004},
+    { title : 'Book one',genre:'fiction',publish:1981,edition:2004},
+    { title : 'Book one',genre:'fiction',publish:1981,edition:2004},
+    { title : 'Book one',genre:'fiction',publish:1981,edition:2004},
+    
+
+ ];
+ const findGenre = books.filter ((fg) => fg.genre === 'non-friction')
+ //console.log(findGenre);
+ const filterData = books.filter((fd) => {return fd.genre === 'history' && fd.edition >=2006})
+ console.log(filterData);
+
+
+
+//set method 
+//how to create a set?
+const mySet = new set();
+//Add element to the set
+mySet.add(4);
+mySet.add(6);
+mySet.add(6);
+mySet.add(7);
+mySet.add(6);
+mySet.add(8);
+mySet.add(7);
+mySet.add(9);
+mySet.add(10);
+console.log(mySet);
+//how to get lengtth or size of set.
+
+const mysetSize = mySet.size;
+console.log(mysetSize);
+
+//Delte an element from the set.
+const deleteElement =(mySet.delete(7));
+console.log(deleteElement)
+console.log(mySet);
+
+//check element is existing or not
+console.log(mySet.has(8));
